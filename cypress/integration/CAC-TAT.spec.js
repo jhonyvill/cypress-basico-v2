984c5579b4b9a536d918837ba62ed32a716a5cca
Jhony Vill <jhonyvill.dev@gmail.com>
2024-02-07 17:24:31 -0300

    move cy.visit() to beforeEach() and create new test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,19 @@
 
 ///<reference types="Cypress"/>
 describe("Customer Service Center TAT", () => {
-  it("check the application title", () => {
+  beforeEach(() => {
     cy.visit("./src/index.html");
+  });
+  it("check the application title", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it("fill in the required fields and submit the form", () => {
+    cy.get('#firstName').type("firstName mock");
+    cy.get('#lastName').type("lastName mock");
+    cy.get('#email').type("mock@email.com");
+    cy.get('#open-text-area').type("text mock");
+    cy.get('button[type="submit"]').click();
+    cy.get('.success').should("be.visible");
   });
 });
